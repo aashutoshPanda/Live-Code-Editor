cc05198175c09905e2a3cf0e78274b0c8ccbea88
Ashutosh Panda <iamashutoshpanda@gmail.com>
2019-03-23 23:32:25 +0530

    first commit

---
+++ javaScript.js
@@ -0,0 +1,39 @@
+	function update(){
+				$("iframe").contents().find("html").html("<html><head><style type='text/css'>"+$("#csspanel").val()+"</style></head><body>"+$(htmlpanel).val()+"</body></html>");
+				
+				document.getElementById("outputpanel").contentWindow.eval($("#javascriptpanel").val());
+			}
+			$(".toggleButtons").hover(
+				
+				function(){
+					$(this).addClass("highlightedButtons");
+			
+				},function(){
+					$(this).removeClass("highlightedButtons")
+				}
+			);
+			$(".toggleButtons").click(function(){
+				$(this).toggleClass("active");
+				$(this).removeClass("highlightedButtons");
+				var panelId="#"+$(this).attr("id")+"panel";
+				$(panelId).toggleClass("hidden");
+				var count_hidden=$(".hidden").length;
+				$(".panel").width(($(window).width()/(4-count_hidden))-15);
+				
+			}
+			);
+
+			
+			$(".panel").height($(window).height()-$("#header").height()-15);
+			
+
+
+
+			$("iframe").contents().find("html").html($("#htmlpanel").val());
+
+
+
+			update();
+			$("textarea").on('change keyup paste', function() {
+				update();
+			});
